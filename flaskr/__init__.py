948dbcf69ade5205dbb9f6c6808d050508e44137
Gian_Pereira <gian.21.otaku@gmail.com>
2025-08-14 09:20:56 -0300

    Making the new application architecture

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
 
 
+def create_app():
+    # Application factory that create the app for us
+    app = Flask(__name__)
+    app.config.from_mapping(
+        SECRET_KEY='dev', #this should be changed later for some random value that's secret, like a env
+    )
 
+    @app.route('/')
+    def hello_world():
+        return "hello world"
+
+    return app
