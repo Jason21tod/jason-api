9c4862c52fb3cf3b8c9c88e8305ffb82d3918250
Gian_Pereira <gian.21.otaku@gmail.com>
2026-04-10 07:21:45 -0300

    First steps at email system

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 import flask 
 import logging
+import smtplib
+import os
+
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
 
 
 def create_app():
@@ -11,5 +18,29 @@ def create_app():
     def home():
         app.logger.info("hello from world")
         return "Salve world!"
+
+    @app.route("/send_test_mail")
+    def send_mail():
+        server = "smtp.gmail.com"
+        port = 587
+        sender_email = str(os.environ.get("EMAIL_SENDER"))
+        password = str(os.environ.get("EMAIL_APP_PASSWORD"))
+        server = smtplib.SMTP(server, port)
+        
+        server.ehlo()
+        server.starttls()
+        server.login(sender_email, password)
+
+        msg = MIMEMultipart()
+        body= "test"
+        msg['Subject'] = "Hello From Test Subject"
+        msg['From'] = sender_email
+        msg['To'] = "gian.21.pereira@gmail.com"
+        msg.attach(MIMEText(body))
+
+        response = str(server.sendmail(sender_email, "gian.21.pereira@gmail.com", msg.as_string()))
+        server.quit()
+        return response
+
 
     return app
