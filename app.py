7d1e2b2027d0cf2ba0665ca4cfd51db9777fe426
Gian_Pereira <gian.21.otaku@gmail.com>
2026-04-28 09:18:51 -0300

    Changed the way of the access of the env - try 2

--- app.py
+++ app.py
@@ -1,8 +1,3 @@
 import app
 
-from dotenv import load_dotenv
-from pathlib import Path
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path, override=True)
 app.create_app().run(host="0.0.0.0", port=8080)
--- app/email_resources.py
+++ app/email_resources.py
@@ -2,16 +2,19 @@
 import smtplib
 import os
 
+from dotenv import dotenv_values
 from flask import current_app
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+values = dotenv_values(".env")
+
 
 _SERVER_ADDRESS = "smtp.gmail.com"
 _PORT = 587
-_SENDER_EMAIL = str(os.environ.get("EMAIL_SENDER"))
-_RECEIVER_EMAIL = str(os.environ.get("EMAIL_RECEIVER"))
-_PASSWORD = str(os.environ.get("EMAIL_APP_PASSWORD"))
+_SENDER_EMAIL = str(values["EMAIL_SENDER"])
+_RECEIVER_EMAIL = str(values["EMAIL_RECEIVER"])
+_PASSWORD = str(values["EMAIL_APP_PASSWORD"])
 
 if _SENDER_EMAIL == "None" or _RECEIVER_EMAIL == "None" or _PASSWORD == "None":
     raise Exception("Error on init: env vars not initialized")
